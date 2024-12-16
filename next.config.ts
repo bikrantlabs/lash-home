3e95f6b394538d6dd59cbd014fb315d5316d6905
Bikrant <bikrantjung1214@gmail.com>
2024-12-16 12:20:17 +0545

    feat: add redirect to lash github page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: process.env.NEXT_PUBLIC_LASH_URL!,
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
