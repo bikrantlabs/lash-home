8fb858eb221706eb983932f15faa436371c35528
Bikrant <bikrantjung1214@gmail.com>
2024-12-16 12:27:02 +0545

    feat: add redirect

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: process.env.NEXT_PUBLIC_LASH_URL!,
-        permanent: false,
-        basePath: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
